ba875d8951da6c44c9cda2143ed00b0d37544f27
Kevin Soffa <kevinsoffa@gmail.com>
2021-11-28 11:51:03 -0300

    Segundo Deploy
    Criando objetos do jogo
    
    Atributos do pássaro (ângulo, velocidade, altura,
    	tempo, contagem da imagem).
    
    Função de pular
    
    Função mover;
    	Calculando o deslocamento
    	Restringindo o deslocamento
    	ângulo do pássaro
    
    	Formula do DESLOCAMENTO;
    	-----------------------
    	S = so + vot + at²/2
    	-----------------------

--- FlappyBird.py
+++ FlappyBird.py
@@ -2,9 +2,17 @@ import pygame
 import os
 import random
 
+"""
+Formula do DESLOCAMENTO;
+-----------------------
+S = so + vot + at²/2
+-----------------------
+"""
+
 LARGURA_TELA = 500
 TELA_ALTURA = 800
 
+# Configurando as imagens
 IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
 IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
@@ -13,5 +21,64 @@ IMAGEM_PASSARO = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
 ]
+
 pygame.font.init()
 FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+
+# Cada Objeto será uma classe
+
+
+class Passaro:
+    IMGS = IMAGEM_PASSARO
+    # animações da rotação
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    # atributos do pássaro
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = IMGS[0]
+
+# Configurações de movimento do pássaro
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        # calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+        # restringer o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        # o ângulo do pássaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+
+
+
+class Cano:
+    pass
+
+
+class Chao:
+    pass
